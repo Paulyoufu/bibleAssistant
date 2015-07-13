0e7efd052456b7f98d0b05a14df8dc598a0adbc6
ca1yufan <765713050@qq.com>
2015-07-13 16:22:37 +0800

    Revert 9737855..ae98a8a
    This rolls back to commit 9737855ac77424d429619a5fabb43b1e9b5e8bd3.

--- client/templates/download/download.js
+++ client/templates/download/download.js
@@ -1,3 +1,39 @@
+Template.download.helpers({
+	booksList: function () {
+		return Session.get('booksList');
+	},
+	bookName: function () {
+		return Session.get('currentBookName'); 
+	},
+	chapterSN: function () {
+		return Session.get('currentChapter');
+	},
+	chapters: function () {
+		var currentChapterCount = Session.get('selectedChapterCount');
+		var chapters = [];
+
+		for (var i = 0; i < currentChapterCount; i++) {
+			var chapterItem = {};
+			chapterItem.chapterSN = i + 1;
+			chapters.push(chapterItem);
+		};
+
+		return chapters;
+	},
+	availWidth: function () {
+		return window.screen.availWidth;
+	}
+	// bookName: function () {
+	// 	return Session.get('currentBookName'); 
+	// },
+	// chapterSN: function () {
+	// 	return Session.get('currentChapter');
+	// },
+	// selectedBookName: function () {
+	// 	return Session.get('selectedBookName');
+	// }
+});
+
 Template.download.events({
 	'click button': function () {
 		Router.go('menu');
