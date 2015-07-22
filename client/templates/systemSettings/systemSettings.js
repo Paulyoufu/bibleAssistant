04e8ba1a21cac0d6d54fadd7aadf25607e2a36a7
wildKids <443468698.com>
2015-07-23 00:03:20 +0800

    播放

--- client/templates/systemSettings/systemSettings.js
+++ client/templates/systemSettings/systemSettings.js
@@ -1,8 +1,41 @@
+Template.systemSettings.rendered = function()
+{
+    if(Session.get('automaticallyDL')){
+        $("#automaticallyDl_id").prop("checked",true); 
+    }
+    if(Session.get('settingFont')){
+        $("#automaticallyDl_id").prop("checked",true); 
+    } 
+};
+
 Template.systemSettings.events({
-	'click button': function () {
-		Router.go('menu');
-	}
+    'click button': function () {
+        Router.go('menu');
+    },'click #automaticallyDl_id' : function() {
+        if($("#automaticallyDl_id").is(':checked')) 
+        {
+            Session.set('automaticallyDL',true);
+            setAutomaticallyDL("true");
+        }else{
+            Session.set('automaticallyDL',false);
+            setAutomaticallyDL("false");
+        }
+    },'click #largeFont_id' : function() {
+        if($("#largeFont_id").is(':checked')) 
+        {        
+            $("#size_Id").addClass("settingFont");
+            Session.set('fontSize',true);
+            setFontSize("true");
+
+        }else{
+            $("#size_Id").removeClass("settingFont");
+            Session.set('fontSize',false);
+            setFontSize("false");
+
+        }
+    }
 });
+
 Template.systemSettings.helpers({
     booksList: function () {
         getBooksList(Session.get("bibleOldOrNew"));
